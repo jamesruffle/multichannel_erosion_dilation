bf84a93b6df18960b2d5cb2b8aec862ac8c5584a
jamesruffle <j.ruffle@ucl.ac.uk>
2024-01-26 21:07:09 +0000

    push v2

--- README.md
+++ README.md
@@ -1,33 +1,9 @@
 # Multichannel erosion and dilation augmentation of 3D masks
 
-These are [MONAI](https://monai.io)-compatible dictionary-based transforms for multi-channel erosion and dilation of masks, for example in the erosion or dilation of synthetic lesions.
+These are [MONAI](https://monai.io)-compatible dictionary-based transforms for erosion and dilation of masks, for example in the erosion or dilation of synthetic lesions. This includes both single (use ```BinaryDilationd``` or ```BinaryErosiond```) and multi-channel lesions (use ```GreyDilationd``` or ```GreyErosiond```).
 
 ## Dictionary transforms
 The transforms are available in [erosion_dilation.py](erosion_dilation.py)
-
-Both BinaryErosiond and BinaryDilationd accept the following arguments:
-```
-Args:
-keys: keys of the corresponding items to be transformed.
-See also: :py:class:`monai.transforms.compose.MapTransform`
-prob: probability of augmentation application.
-(Default 0.1, with 10% probability that Binary Erosion will occur)
-iteration: Specify a specific number of iterations to be run.
-(Default 1)
-random_iteration: Specify to take a random number of iterations between the range iteration_low, iteration_high
-(Default True)
-iteration_low: lower limit for the random integer selection for number of iterations for Binary Erosion
-(Default 1)
-iteration_high: upper limit for the random integer selection for number of iterations for Binary Erosion
-(Default 5)
-fill_holes: whether to fill holes of output mask (especially helpful for boundaries with multilabel masks)
-(Default False)
-fill_holes_iterations: how many iterations of how filling to perform
-(Default 1 iterations)
-allow_missing_keys: don't raise exception if key is missing.
-verbose: enable verbose mode for debugging.
-seed: random seed for the generator.
-```
 
 ## Example use
 A Jupyter Notebook with example use case for a multi-channel brain tumour segmentation is available in [example_use.ipynb](example_use.ipynb). A sample image of this is shown below.
--- erosion_dilation.py
+++ erosion_dilation.py
@@ -18,6 +18,7 @@ from monai.transforms.transform import MapTransform, RandomizableTransform
 from monai.config.type_definitions import NdarrayOrTensor
 import scipy
 import numpy as np
+import warnings
 
 class BinaryErosiond(RandomizableTransform, MapTransform):
     """
@@ -123,6 +124,9 @@ class BinaryErosiond(RandomizableTransform, MapTransform):
                 return d
 
             if self._do_transform:
+                if len(self.keys)>1:
+                    warnings.warn('Warning...using dictionary transform intended for single channel data with multi-channel data, this may degrade outputs. Consider using GreyErosiond instead here')             
+                
                 if self.verbose:
                     print('Doing transform')
                 for key in self.keys:
@@ -251,6 +255,9 @@ class BinaryDilationd(RandomizableTransform, MapTransform):
                 return d
 
             if self._do_transform:
+                if len(self.keys)>1:
+                    warnings.warn('Warning...using dictionary transform intended for single channel data with multi-channel data, this may degrade outputs. Consider using GreyDilationd instead here')             
+ 
                 if self.verbose:
                     print('Doing transform')
                 for key in self.keys:
@@ -274,3 +281,288 @@ class BinaryDilationd(RandomizableTransform, MapTransform):
                                 d[key].array = scipy.ndimage.binary_fill_holes(d[key])
                             
             return d
+        
+        
+class GreyDilationd(RandomizableTransform, MapTransform):
+    """
+    Dictionary-based version for Grey Dilation transform:py:class:
+    Author: James Ruffle | j.ruffle@ucl.ac.uk | 25/1/24
+    """
+    def __init__(
+        self,
+        keys: KeysCollection,
+        prob: float = 0.1,
+        size: int = 1,
+        random_size: bool = True,
+        size_low: int = 1,
+        size_high: int = 5,
+        randomize_key_order: bool = True,
+        allow_missing_keys: bool = True,
+        verbose: bool = False,
+        seed: int = 0,
+        
+    ) -> None:
+        """
+        Args:
+            keys: keys of the corresponding items to be transformed.
+                See also: :py:class:`monai.transforms.compose.MapTransform`
+            prob: probability of augmentation application.
+                (Default 0.1, with 10% probability that Grey Dilation will occur)
+            size: Specify a specific size of dilation to be run.
+                (Default 1)
+            random_size: Specify to take a random number of dilation between the range size_low, size_high
+                (Default True)
+            size_low: lower limit for the random integer selection for size for Grey Dilation
+                (Default 1)
+            size_high: upper limit for the random integer selection for size for Grey Dilation
+                (Default 5)
+            randomize_key_order: whether to randomize the key order for the operation, this will affect the direction of dilation or erosion and randomize the augmentation even further.
+                (Default True)
+            allow_missing_keys: don't raise exception if key is missing.
+            verbose: enable verbose mode for debugging.
+            seed: random seed for the generator.
+        """
+        
+        MapTransform.__init__(self, keys, allow_missing_keys)
+        RandomizableTransform.__init__(self,prob=prob)
+        
+        self.keys = keys
+        self.allow_missing_keys = allow_missing_keys
+        self.size = size
+        self.random_size = random_size
+        self.size_low = size_low
+        self.size_high = size_high
+        self.randomize_key_order = randomize_key_order
+        self.verbose = verbose
+        self.seed = seed
+
+    def set_random_state(
+        self, seed: Optional[int] = None, state: Optional[np.random.RandomState] = None
+    ) -> "BinaryDilationd":
+        super().set_random_state(self.seed, state)
+        return self
+    
+    def randomize(self: Optional[Any] = None) -> None:
+        super().randomize(None)
+        if not self._do_transform:
+            return None
+        if self.random_size and self._do_transform:
+            self.size = self.R.randint(low=self.size_low, high=self.size_high)
+            if self.verbose:
+                print('Choosing random size within the low to high range')
+        if not self.random_size and self._do_transform:
+            if self.verbose:
+                print('Using fixed number of iterations')
+        if self.verbose:
+            print('Chosen size: '+str(self.size))
+
+    def __call__(self, data: Mapping[Hashable, NdarrayOrTensor]) -> Dict[Hashable, NdarrayOrTensor]:
+        d = dict(data)
+        keys_to_process = list(set(list(d)).intersection(set(list(self.keys))))
+        if len(keys_to_process) == 0:
+            """
+            This is only a problem if we do not allow missing keys
+            """
+            if not self.allow_missing_keys:
+                if self.verbose:
+                    print("No keys to process. Exiting.")
+            else:
+                return d
+        else:
+
+            self.randomize()
+            
+            if not self._do_transform:
+                if self.verbose:
+                    print('Not doing transform')
+                return d
+
+            if self._do_transform:
+                if self.verbose:
+                    print('Doing transform')
+                
+                key_dict = dict()
+                key_dict_counter=1
+                
+                for key in self.keys:
+                    if key not in d and not self.allow_missing_keys:
+                        if self.verbose:
+                            print("key not found. Exiting.")
+                        quit()
+                    if key in d:
+                        if key_dict_counter==1:
+                            multi_key = np.zeros(shape=d[key].array.shape)
+                        
+                        if self.randomize_key_order:
+                            key_dict_value = np.random.randint(999)
+                        if not self.randomize_key_order:
+                            key_dict_value = key_dict_counter
+                        key_dict[key]=key_dict_value #create a dictionary for values
+                        if self.verbose:
+                            print('current key: '+str(key))
+                            print("current key-dict: "+str(key_dict))
+                            print('current key_dict_counter: '+str(key_dict_counter))
+                        multi_key = np.where(d[key].array>0,key_dict[key],multi_key)
+                        key_dict_counter+=1
+                if self.verbose:
+                    print('multi_key_complete, unique values: '+str(np.unique(multi_key)))
+                multi_key = scipy.ndimage.grey_dilation(multi_key,size=(self.size,self.size,self.size))
+                
+                if self.verbose:
+                    print('grey process complete, now recasting to individual keys')
+                for key in self.keys:
+                    if key not in d and not self.allow_missing_keys:
+                        if self.verbose:
+                            print("key not found. Exiting.")
+                        quit()
+                    if key in d:
+                        d[key].array=np.where(multi_key==key_dict[key],1,0)
+                        if self.verbose:
+                            print('current key: '+str(key))
+                            print("current key-dict_val: "+str(key_dict[key]))
+                            print('unique values in d[key]: '+str(np.unique(d[key])))
+              
+        return d
+    
+    
+class GreyErosiond(RandomizableTransform, MapTransform):
+    """
+    Dictionary-based version for Grey Erosion transform:py:class:
+    Author: James Ruffle | j.ruffle@ucl.ac.uk | 25/1/24
+    """
+    def __init__(
+        self,
+        keys: KeysCollection,
+        prob: float = 0.1,
+        size: int = 1,
+        random_size: bool = True,
+        size_low: int = 1,
+        size_high: int = 5,
+        randomize_key_order: bool = True,
+        allow_missing_keys: bool = True,
+        verbose: bool = False,
+        seed: int = 0,
+        
+    ) -> None:
+        """
+        Args:
+            keys: keys of the corresponding items to be transformed.
+                See also: :py:class:`monai.transforms.compose.MapTransform`
+            prob: probability of augmentation application.
+                (Default 0.1, with 10% probability that Grey Erosion will occur)
+            size: Specify a specific size of dilation to be run.
+                (Default 1)
+            random_size: Specify to take a random number of erosion between the range size_low, size_high
+                (Default True)
+            size_low: lower limit for the random integer selection for size for Grey Erosion
+                (Default 1)
+            size_high: upper limit for the random integer selection for size for Grey Erosion
+                (Default 5)
+            randomize_key_order: whether to randomize the key order for the operation, this will affect the direction of dilation or erosion and randomize the augmentation even further.
+                (Default True)
+            allow_missing_keys: don't raise exception if key is missing.
+            verbose: enable verbose mode for debugging.
+            seed: random seed for the generator.
+        """
+        
+        MapTransform.__init__(self, keys, allow_missing_keys)
+        RandomizableTransform.__init__(self,prob=prob)
+        
+        self.keys = keys
+        self.allow_missing_keys = allow_missing_keys
+        self.size = size
+        self.random_size = random_size
+        self.size_low = size_low
+        self.size_high = size_high
+        self.randomize_key_order = randomize_key_order
+        self.verbose = verbose
+        self.seed = seed
+
+    def set_random_state(
+        self, seed: Optional[int] = None, state: Optional[np.random.RandomState] = None
+    ) -> "BinaryDilationd":
+        super().set_random_state(self.seed, state)
+        return self
+    
+    def randomize(self: Optional[Any] = None) -> None:
+        super().randomize(None)
+        if not self._do_transform:
+            return None
+        if self.random_size and self._do_transform:
+            self.size = self.R.randint(low=self.size_low, high=self.size_high)
+            if self.verbose:
+                print('Choosing random size within the low to high range')
+        if not self.random_size and self._do_transform:
+            if self.verbose:
+                print('Using fixed number of iterations')
+        if self.verbose:
+            print('Chosen size: '+str(self.size))
+
+    def __call__(self, data: Mapping[Hashable, NdarrayOrTensor]) -> Dict[Hashable, NdarrayOrTensor]:
+        d = dict(data)
+        keys_to_process = list(set(list(d)).intersection(set(list(self.keys))))
+        if len(keys_to_process) == 0:
+            """
+            This is only a problem if we do not allow missing keys
+            """
+            if not self.allow_missing_keys:
+                if self.verbose:
+                    print("No keys to process. Exiting.")
+            else:
+                return d
+        else:
+
+            self.randomize()
+            
+            if not self._do_transform:
+                if self.verbose:
+                    print('Not doing transform')
+                return d
+
+            if self._do_transform:
+                if self.verbose:
+                    print('Doing transform')
+                
+                key_dict = dict()
+                key_dict_counter=1
+                
+                for key in self.keys:
+                    if key not in d and not self.allow_missing_keys:
+                        if self.verbose:
+                            print("key not found. Exiting.")
+                        quit()
+                    if key in d:
+                        if key_dict_counter==1:
+                            multi_key = np.zeros(shape=d[key].array.shape)
+                        
+                        if self.randomize_key_order:
+                            key_dict_value = np.random.randint(999)
+                        if not self.randomize_key_order:
+                            key_dict_value = key_dict_counter
+                        key_dict[key]=key_dict_value #create a dictionary for values
+                        
+                        if self.verbose:
+                            print('current key: '+str(key))
+                            print("current key-dict: "+str(key_dict))
+                            print('current key_dict_counter: '+str(key_dict_counter))
+                        multi_key = np.where(d[key].array>0,key_dict[key],multi_key)
+                        key_dict_counter+=1
+                if self.verbose:
+                    print('multi_key_complete, unique values: '+str(np.unique(multi_key)))
+                multi_key = scipy.ndimage.grey_erosion(multi_key,size=(self.size,self.size,self.size))
+                
+                if self.verbose:
+                    print('grey process complete, now recasting to individual keys')
+                for key in self.keys:
+                    if key not in d and not self.allow_missing_keys:
+                        if self.verbose:
+                            print("key not found. Exiting.")
+                        quit()
+                    if key in d:
+                        d[key].array=np.where(multi_key==key_dict[key],1,0)
+                        if self.verbose:
+                            print('current key: '+str(key))
+                            print("current key-dict_val: "+str(key_dict[key]))
+                            print('unique values in d[key]: '+str(np.unique(d[key])))
+              
+        return d
